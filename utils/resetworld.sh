80c2157bb453606de89d6ed06243178f533f2ebc
mark.carline@btinternet.com <mark.carline@btinternet.com>
2018-02-28 12:03:24 +0000

    Added minecraft world restore script

---
+++ utils/resetworld.sh
@@ -0,0 +1,2 @@
+rm -r /home/pi/.minecraft/games/com.mojang/minecraftWorlds/world
+cp -r /home/pi/.minecraft/games/com.mojang/minecraftWorlds/world.backup /home/pi/.minecraft/games/com.mojang/minecraftWorlds/world

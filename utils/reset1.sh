c5af1fb6e118e1fbcd8a0ed159d3777a2de53767
Mark Carline <mark.carline@btinternet.com>
2019-04-16 19:35:37 +0100

    added sudo to ensure deleted

--- utils/reset1.sh
+++ utils/reset1.sh
@@ -5,7 +5,7 @@ echo Pulling down latest copy of CoderDojo from github ...
 git clone https://github.com/markcarline/CoderDojo /home/pi/CoderDojo
 
 echo Removing old BACKUP CoderDojo folder ...
-rm -r -f /home/pi/BACKUP
+sudo rm -r -f /home/pi/BACKUP
 
 echo Creating a backup of /home/pi/CoderDojo as /home/pi/BACKUP
 cp -r /home/pi/CoderDojo /home/pi/BACKUP
